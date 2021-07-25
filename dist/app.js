4b53cb349b4717d2f947bbdbc75b08c259e4ccc9
Nishant <nishantbhosale244@gmail.com>
2021-07-25 08:57:10 +0530

    Advanced Types and type guards

--- dist/app.js
+++ dist/app.js
@@ -12,3 +12,41 @@ let user1;
 user1 = new AnotherPerson("Harshad");
 console.log(user1.age);
 user1.greet(`Hello`);
+function printEmployeeInfo(e1) {
+    console.log(e1.name);
+    if ("privileges" in e1) {
+        console.log(e1.privileges);
+    }
+    if ("startDate" in e1) {
+        console.log(e1.startDate);
+    }
+}
+const newEmployee = {
+    name: "Max",
+    startDate: new Date(),
+    privileges: ["everything"],
+};
+printEmployeeInfo(newEmployee);
+class Car {
+    drive() {
+        console.log("Driving....");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving a truck...");
+    }
+    loadCargo(amount) {
+        console.log("Loaded Cargo = " + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- src/app.ts
+++ src/app.ts
@@ -24,3 +24,75 @@ let user1;
 user1 = new AnotherPerson("Harshad");
 console.log(user1.age);
 user1.greet(`Hello`);
+
+type Combinables = string | number;
+type Numeric = number | boolean;
+
+//Intersection of two union types
+type universal = Combinables & Numeric;
+
+type Employee = {
+	name: string;
+	startDate: Date;
+};
+
+type Admin = {
+	name: string;
+	privileges: string[];
+};
+
+type elevatedEmployee = Admin & Employee;
+
+//Intersection of Employee and Admin
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo(e1: UnknownEmployee): void {
+	console.log(e1.name);
+
+	if ("privileges" in e1) {
+		console.log(e1.privileges);
+	}
+	if ("startDate" in e1) {
+		console.log(e1.startDate);
+	}
+}
+
+const newEmployee: UnknownEmployee = {
+	name: "Max",
+	startDate: new Date(),
+	privileges: ["everything"],
+};
+
+printEmployeeInfo(newEmployee);
+
+class Car {
+	drive() {
+		console.log("Driving....");
+	}
+}
+
+class Truck {
+	drive() {
+		console.log("Driving a truck...");
+	}
+
+	loadCargo(amount: number) {
+		console.log("Loaded Cargo = " + amount);
+	}
+}
+
+const v1 = new Car();
+const v2 = new Truck();
+
+type Vehicle = Car | Truck;
+
+function useVehicle(vehicle: Vehicle) {
+	vehicle.drive();
+
+	if (vehicle instanceof Truck) {
+		vehicle.loadCargo(1000);
+	}
+}
+
+useVehicle(v1);
+useVehicle(v2);
